3e2080324cc780e5c054113bc11e8e7df611f142
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-02-05 23:29:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,7 +9,11 @@ def directors_totals(nds)
     result[directors_database[index][:name]] = 0
     count = 0 
     
-    #while count < directors_database[index]
+    while count < directors_database[index][:name][:movies].length
+    
+      result[directors_database][index][:name] += directors_database[index][:name][movies][count][:worldwide_gross]
+      count += 1
+      end 
     index += 1
   end
 return result  
